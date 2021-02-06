343f9b54af84848d0e375ea5bf65ab2ba28b1c85
Rasengangstarr <davidouthere@msn.com>
2021-02-06 11:55:04 +0000

    drawing logic working

--- main.py
+++ main.py
@@ -7,18 +7,34 @@ import math
 
 
 def redraw(stdscr, t, notes):
-    notesToDraw = [n for n in notes if n[1]/1000 < t + 3 and n[1]/1000 > t]
-    notesOnAdjustedLines = [(n[0], math.ceil((n[1]-t*1000) / 30), n[2]) for n in notesToDraw]
+    notesToDraw = [n for n in notes if n[1]/1000 <= t + 3 and n[1]/1000 >= t-1]
+    notesOnAdjustedLines = [(n[0], math.ceil((n[1]-t*1000) / 90), n[2]) for n in notesToDraw]
     stdscr.addstr(31,0,str(notesToDraw))
     stdscr.addstr(32,0,str(t))
     stdscr.addstr(33,0,str(notesOnAdjustedLines))
-    for i in range (0, 30):
-        noteToDraw = [n for n in notesOnAdjustedLines if n[1] == i]
-        if len(noteToDraw) > 0:
-            stdscr.addstr(i,0,str(noteToDraw[0][1] ))
+    for i in range (0, 30, 1):
+        stdscr.addstr(35,0,str(i))
+        
+        notesToDraw = [n[0] for n in notesOnAdjustedLines if n[1] == i]
+        
+        if i != 0:
+            lineString = "|"
+            for j in range (1,6):
+                if j in notesToDraw:
+                    lineString += "| X |"
+                else:
+                    lineString += "|   |"
+            lineString += "|"
         else:
-            stdscr.addstr(i,0,"nout")
-
+            lineString = "["
+            for j in range (1,6):
+                if j in notesToDraw:
+                    lineString += "[ X ]"
+                else:
+                    lineString += "[   ]"
+            lineString += "]"
+        
+        stdscr.addstr(30-i, 0, lineString)
 
 
 
@@ -30,7 +46,9 @@ curses.noecho()
 stdscr.nodelay(1) # set getch() non-blocking
 
 # note, start, finish
-notes = [(1, 5000, 5000)]
+notes = [(1, 5000, 5000),(2,3000,3000), (4,4000,4000)]
+
+rrate = 0.01
 
 stdscr.addstr(0,0,"Press \"p\" to show count, \"q\" to exit...")
 line = 1
@@ -46,7 +64,7 @@ try:
         #    line += 1
         if c == ord('x'):
             break
-        if now - lastDraw > 0.01:
+        if now - lastDraw > rrate:
             stdscr.addstr(0,0,str(now - lastDraw))
             
             lastDraw = now
